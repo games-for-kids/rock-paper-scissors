f180300ddffd80d43d3f1fc727d6240bf27c013c
Alon Nativ <alon.nativ@gmail.com>
2018-08-04 21:10:11 +0300

    add curl example

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import io from 'socket.io-client';
 import logo from './logo.svg';
 import './App.css';
 
+const DEV_SERVER = "http://localhost:5000";
+const PROD_SERVER = 'https://the-game-rock-paper-scissors.herokuapp.com';
+
 const PAPER = "✋";
 const ROCK = "👊";
 const SCISSORS = "✌️";
@@ -20,36 +23,36 @@ class App extends Component {
     }
     this.socket.on("play", (data) => {
       console.log('got data', data)
-      this.setState({data: data, round: this.state.round+1 });
+      this.setState({ data: data, round: this.state.round + 1 });
     })
     //to keep the connection alive in heroku env
-    setInterval(()=> this.socket.send('keepAlive',{t: Date.now()}, (msg)=>console.log(msg)), 25 * 1000);
+    setInterval(() => this.socket.send('keepAlive', { t: Date.now() }, (msg) => console.log(msg)), 25 * 1000);
   }
 
   render() {
-    const {myMove, whoWon, command} = this.state.data;
+    const { myMove, whoWon, command } = this.state.data;
     const emoji = this.getEmoji(myMove);
     if (!myMove) {
       return <div>
         <h1>Watting for your move!</h1>
         <h2>Commands (dev)</h2>
         <ul>
-          <li>http://localhost:5000/play/rock</li>
-          <li>http://localhost:5000/play/paper</li>
-          <li>http://localhost:5000/play/scissors</li>
+          <GameLink host={DEV_SERVER} move="rock"/>
+          <GameLink host={DEV_SERVER} move="paper"/>
+          <GameLink host={DEV_SERVER} move="scissors"/>        
         </ul>
         <h2>Commands (production)</h2>
         <ul>
-          <li>/play/rock</li>
-          <li>/play/paper</li>
-          <li>/play/scissors</li>
+          <GameLink host={PROD_SERVER} move="rock"/>
+          <GameLink host={PROD_SERVER} move="paper"/>
+          <GameLink host={PROD_SERVER} move="scissors"/>
         </ul>
       </div>
     }
     return (
       <div>
         <div>Round: {this.state.round}</div>
-        <div style={{fontSize: "260px"}}>{emoji}</div>
+        <div style={{ fontSize: "260px" }}>{emoji}</div>
         <div>You played: {command} </div>
         <h1>{whoWon} WON!</h1>
       </div>
@@ -62,5 +65,15 @@ class App extends Component {
     if (myMove == "scissors") return SCISSORS;
   }
 }
+
+const GameLink = ({ host,  move }) => {
+  const url = `${host}/play/${move}`
+  return (
+    <li>
+      <a href={url} target={move}>{`curl '${url}'`}</a>
+    </li>
+  )
+}
+
 
 export default App;
